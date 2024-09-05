09ffc12cfdf875fa3bdaa7f87b288ebd1c0c76f3
sdiehl717 <sdiehl12345@gmail.com>
2024-09-04 20:34:25 -0400

    Initial commit

---
+++ problems/03-log-between.js
@@ -0,0 +1,22 @@
+/*
+Define a function logBetween that takes in two number parameters. The first
+number parameter represents a low number and the second represents a high
+number. The function should print every number from the low number to the
+high number, inclusive. Inclusive means that the range includes the low and
+high numbers.
+*/
+
+// Your code here
+function logBetween(lowNum, highNum) {
+  for (let i = lowNum; i <= highNum; i++) console.log(i);
+}
+logBetween(-1, 2);
+/* prints out:
+-1
+0
+1
+2
+*/
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+module.exports = logBetween;
